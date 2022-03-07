387c4b8a0a9901e124e4bd3c84522705236b5c47
Caio <whoiscaio@protonmail.com>
2022-03-07 16:29:29 -0300

    feat: socket server set

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,18 @@
 import express from 'express';
 import http from 'http';
-import io from 'socket.io';
+import { Server } from 'socket.io';
 import { config } from 'dotenv';
+
 config();
 
 const PORT = process.env.PORT || 3001;
+const FE_PORT = process.env.FE_PORT || 3000;
 
 const app = express();
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: `http://localhost:${FE_PORT}` } })
+
+server.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
 })
